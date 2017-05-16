a3d91b27afea9c5ac34e92d9f1c74f16fc3b6497
Giovana Ansoain <giovanaansoain@gmail.com>
2017-05-16 18:11:57 -0300

    Ajustes Slider
    Slide funcionando pra avançar e voltar

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
-$( document ).ready(function() {
+$(document).ready(function(){
 
-  var slides = $("#slides");
+  var slideDiv = $("#slides");
+  var slides = $(".slide");
+  var slidesLength = slides.length;
 
-  function avancarSlide() {
-    slides.css("transform", "translateX(-100vw)");
+  $(".seta").click(mudarSlide);
+
+  function mudarSlide(evento){
+    var direcaoSeta = evento.currentTarget.classList[3];
+    var slideAtivo = $(".active");
+    var dataSlideAtivo = parseInt(slideAtivo["0"].dataset.slide);
+
+    if (direcaoSeta == "direita") {
+      if (dataSlideAtivo >= slidesLength-1) {
+        translateSlide(0);
+      } else {
+        translateSlide(dataSlideAtivo+1);
+      }
+    } else {
+      if(dataSlideAtivo <= 0){
+        translateSlide(slidesLength-1);
+      } else {
+        translateSlide(dataSlideAtivo-1);
+      }
+    }
+
+    function translateSlide(slideSeguinte){
+      slideDiv.css("transform", "translateX(-" + slideSeguinte + "00vw)");
+      slideAtivo.removeClass("active");
+      $("[data-slide=" + slideSeguinte + "]").addClass("active");
+    }
+
+
+
   }
-
-  var setaDireita = $(".seta.direita");
-  setaDireita.click(avancarSlide);
-
-
-
-
-
-
-
 
 });
